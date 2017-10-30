47382dfa971476b511050e29ff4d14d120f94fa8
Jibril <j.d.ivan69@gmail.com>
2017-10-29 23:05:06 -0600

    Unable to put youtube fragment player.

--- Mindset/app/src/main/java/itesm/mx/mindset/RestFragment.java
+++ Mindset/app/src/main/java/itesm/mx/mindset/RestFragment.java
@@ -6,19 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RestFragment extends Fragment {
 
+//    public static final String DEVELOPER_KEY = "AIzaSyAy0CvwCL4O1yHH682zdFQBX2Z3gWra6kI";
+//    public ImageView ytMed;
 
     public RestFragment() {
         // Required empty public constructor
     }
 
-    public static Fragment newInstance(){
+    public static RestFragment newInstance(){
 
         return new RestFragment();
     }
@@ -27,7 +29,10 @@ public class RestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+//        ytMed = (ImageView) container.findViewById(R.id.image_meditation);
+
         return inflater.inflate(R.layout.fragment_rest, container, false);
     }
-
 }
+
